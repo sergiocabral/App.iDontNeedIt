6b301d84414aadd712e5dbde19790e7f144a6aec
Sergio Cabral <git@sergiocabral.com>
2025-07-08 00:00:31 -0300

    feat(i18n): translate open graph metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import '../globals.css'
 import { hasLocale, NextIntlClientProvider } from 'next-intl'
 import { notFound } from 'next/navigation'
 import { routing } from '@/i18n/routing'
+import { getTranslations } from 'next-intl/server'
 
 const copse = Copse({
   weight: '400',
@@ -13,30 +14,34 @@ const copse = Copse({
   display: 'swap',
 })
 
-export const metadata: Metadata = {
-  title: "I Don't Need It",
-  description: 'Are you richer than me? Prove it. Show off your wealth, if you really have it.',
-  keywords: ['money', 'ego', 'luxury', 'wealth', 'flex', 'rich', 'challenge'],
-  authors: [{ name: 'qynea' }],
-  creator: 'qynea',
-  metadataBase: new URL('https://idontneedit.org'),
-  openGraph: {
-    title: "I Don't Need It",
-    description: 'Are you richer than me? Prove it. Show off your wealth, if you really have it.',
-    url: 'https://idontneedit.org',
-    siteName: "I Don't Need It",
-    locale: 'en_US',
-    type: 'website',
-    images: ['/opengraph-image.png'],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: "I Don't Need It",
-    description: 'Are you richer than me? Prove it.',
-    site: '@qynea',
-    creator: '@qynea',
-    images: ['/opengraph-image.png'],
-  },
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string }
+}): Promise<Metadata> {
+  const t = await getTranslations({ locale: params.locale, namespace: 'Metadata' })
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    openGraph: {
+      title: t('title'),
+      description: t('description'),
+      url: 'https://idontneedit.org',
+      siteName: t('title'),
+      locale: params.locale.replace('-', '_'),
+      type: 'website',
+      images: ['/opengraph-image.png'],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('title'),
+      description: t('twitter'),
+      site: '@qynea',
+      creator: '@qynea',
+      images: ['/opengraph-image.png'],
+    },
+  }
 }
 
 export default async function LocaleLayout({
