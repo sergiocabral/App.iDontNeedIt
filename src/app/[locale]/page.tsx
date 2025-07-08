95f55e0763f854b75e8eccab24f684e8955f226d
Sergio Cabral <git@sergiocabral.com>
2025-07-08 17:08:39 -0300

    fix: adjust front page theme

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-center overflow-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground text-center overflow-hidden">
       <Image
         src="/img/logo.png"
         alt="idontneedit logo"
