ae675d3fa159d06cf401dee4a8a042592722305f
Sergio Cabral <git@sergiocabral.com>
2025-07-08 16:00:16 -0300

    feat: api using aiGenerators

--- src/app/api/generate-message/route.ts
+++ src/app/api/generate-message/route.ts
@@ -1,16 +1,22 @@
-import { askAi } from '@/lib/askAi'
+import { generateConfidenceBoost } from '@/lib/aiGenerators'
 import { getDefinitions } from '@/lib/definitions'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const def = getDefinitions()
-  const body = await req.json()
+
+  let body: Record<string, any>
+  try {
+    body = await req.json()
+  } catch (e) {
+    body = {}
+  }
+
   const headerLocale = req.headers.get('accept-language')?.split(',')[0]?.split('-')[0]
   const locale =
     body.locale || headerLocale || process.env.NEXT_PUBLIC_DEFAULT_LOCALE || def('defaultLocale')
 
-  const prompt = `Responda em ${locale}. Gere uma frase provocadora e impactante, com até 15 palavras, que pareça dita por alguém vaidoso, rico, prepotente ou excêntrico. Evite palavrões. Estilo debochado, alinhado com a ideia de alguém que pagou para se exibir publicamente.`
-  console.log(prompt)
-  const message = await askAi(prompt)
-  return NextResponse.json({ message })
+  const values = await generateConfidenceBoost(locale)
+
+  return NextResponse.json(values)
 }
--- src/app/api/generate-name/route.ts
+++ src/app/api/generate-name/route.ts
@@ -1,16 +1,22 @@
-import { askAi } from '@/lib/askAi'
+import { generatePersonsNames } from '@/lib/aiGenerators'
 import { getDefinitions } from '@/lib/definitions'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const def = getDefinitions()
-  const body = await req.json()
+
+  let body: Record<string, any>
+  try {
+    body = await req.json()
+  } catch (e) {
+    body = {}
+  }
+
   const headerLocale = req.headers.get('accept-language')?.split(',')[0]?.split('-')[0]
   const locale =
     body.locale || headerLocale || process.env.NEXT_PUBLIC_DEFAULT_LOCALE || def('defaultLocale')
 
-  const prompt = `Gere no idioma do locale ${locale} um nome anônimo criativo com 2 palavras, que soe como o apelido de alguém excêntrico e rico. Pode parecer nome de usuário, codinome ou apelido. Evite números, use apenas letras.`
+  const values = await generatePersonsNames(locale)
 
-  const name = await askAi(prompt)
-  return NextResponse.json({ name })
+  return NextResponse.json(values)
 }
